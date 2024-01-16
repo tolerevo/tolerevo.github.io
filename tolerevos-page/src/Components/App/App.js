0d3889978191185a4e0e76fad1b8dce6c6a52989
tolerevo <tolerevo@hotmail.com>
2024-01-16 09:37:34 -0600

    mend

--- tolerevos-page/src/Components/App/App.js
+++ tolerevos-page/src/Components/App/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
+import IndexPage from "../IndexPage/IndexPage";
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <>
+      <IndexPage />
+    </>
   );
 }
 
---
+++ tolerevos-page/src/Components/IndexPage/IndexPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./IndexPage.css";
+
+const IndexPage = () => {
+  return (
+    <header>
+      <h1>Diego Tolentino</h1>
+    </header>
+  );
+};
+
+export default IndexPage;
